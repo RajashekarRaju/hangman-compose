307a70d3c8d4e9c9028b3182ad760f66d738e73a
rajas <rajashekarraju123@gmail.com>
2022-04-10 07:22:36 +0530

    Added extra data class property to Alphabet to keep track of player guessed and not guessed letters shown in screen. Merged GameDifficulty.kt into utils file.

--- app/src/main/java/com/hangman/hangman/modal/Alphabets.kt
+++ app/src/main/java/com/hangman/hangman/modal/Alphabets.kt
@@ -1,10 +1,9 @@
 package com.hangman.hangman.modal
 
-import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
 
-@Immutable
 data class Alphabets(
     @Stable val alphabetId: Int,
-    val alphabet: String
+    val alphabet: String,
+    var isAlphabetGuessed: Boolean = false
 )
--- app/src/main/java/com/hangman/hangman/utils/GameDifficultyFilterUitls.kt
+++ app/src/main/java/com/hangman/hangman/utils/GameDifficultyFilterUitls.kt
@@ -3,26 +3,48 @@ package com.hangman.hangman.utils
 import com.hangman.hangman.repository.GameData
 import com.hangman.hangman.repository.database.entity.WordsEntity
 
+/**
+ * [EASY] -> Filters words of length 4 & 5.
+ * [MEDIUM] -> Filters words of length 6 & 7
+ * [HARD] -> Filters words of length 8 to 10
+ */
+enum class GameDifficulty {
+    EASY, MEDIUM, HARD
+}
 
+/**
+ * Filter between three different guessing words lists based on [GameDifficulty].
+ */
 fun getFilteredWordsByGameDifficulty(
     gameDifficulty: GameDifficulty
 ): List<WordsEntity> {
     val wordsList = ArrayList<WordsEntity>()
+    // First get the result list from difficulty and map those to WordsEntity.
     with(filterWordsByDifficulty(gameDifficulty)) {
         this.map { word ->
-            wordsList.add(WordsEntity(wordName = word))
+            wordsList.add(
+                WordsEntity(wordName = word)
+            )
         }
     }
 
+    // Make sure to return only 5 shuffled words instead of whole list.
     return getFiveUniqueGuessingWords(wordsList)
 }
 
+/**
+ * Return only 5 words from the whole list of guessing words.
+ * Since we will be having 5 levels per each game, one word per level.
+ */
 private fun getFiveUniqueGuessingWords(
     wordsList: ArrayList<WordsEntity>
 ): List<WordsEntity> {
     return wordsList.shuffled().take(5)
 }
 
+/**
+ * Determine current difficulty to get correct list from the object [GameData].
+ */
 private fun filterWordsByDifficulty(
     gameDifficulty: GameDifficulty,
     data: GameData = GameData
