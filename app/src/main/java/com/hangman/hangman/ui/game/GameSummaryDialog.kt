e56de2bbec798713c8dd59922c3d52ae779400dc
rajas <rajashekarraju123@gmail.com>
2022-04-16 19:44:22 +0530

    Replaced existing background image with newly created transparent image to match for both teal/red theme colors. Used color filters to match images by selected theme.

--- app/src/main/java/com/hangman/hangman/ui/game/GameSummaryDialog.kt
+++ app/src/main/java/com/hangman/hangman/ui/game/GameSummaryDialog.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -112,6 +113,7 @@ fun ShowPopupWhenGameLost(
                 densityValue = (-400).dp,
                 content = {
                     Image(
+                        colorFilter = ColorFilter.tint(color = MaterialTheme.colors.primary),
                         painter = painterResource(id = R.drawable.rope_empty_title),
                         contentDescription = stringResource(R.string.cd_game_lost_hangman_image),
                         alpha = 0.75f,
@@ -124,6 +126,7 @@ fun ShowPopupWhenGameLost(
             )
 
             Image(
+                colorFilter = ColorFilter.tint(color = MaterialTheme.colors.primary),
                 painter = painterResource(id = R.drawable.evil_hand),
                 contentDescription = stringResource(R.string.cd_game_lost_hangman_image),
                 modifier = Modifier.align(alignment = Alignment.BottomCenter),
